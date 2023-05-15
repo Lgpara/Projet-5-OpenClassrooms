ee20845573f7b9e8d6dc2c71d4807cf86e84b689
Louis Gerber <louisgerber004@gmail.com>
2023-05-15 12:36:34 +0200

    JS code carousel done

--- assets/script.js
+++ assets/script.js
@@ -16,34 +16,99 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+// Constante et variables 
+
 const right = document.querySelectorAll(".arrow_right")
 const left = document.querySelectorAll(".arrow_left")
+
+const dotsContainer = document.querySelector(".dots");
+
 let background_slide = 0;
 let taille_tab = (slides.length - 1) 
 
+//Fonctions
+
+function changeBG(background_slide) {
+	let slideSelector = document.querySelector (".banner-img")
+	let slideIndex = slides[background_slide]
+	let imageSelect = slideIndex.image
+	slideSelector.src = (`./assets/images/slideshow/${imageSelect}`)
+	document.getElementById("tag_line").innerHTML = slides[background_slide].tagLine
+}
+
+function selectedDot(background_slide) {
+		for (let i = 0; i < dotsContainer.children.length; i++) {
+		if (i == background_slide) {
+			dotsContainer.children[i].classList.add('dot_selected');
+		}
+		if (i !== background_slide) {
+			dotsContainer.children[i].classList.remove('dot_selected');			
+		}
+	}
+}
+
+// Event Listener For Navigation Arrows
+
 right.forEach(right => {
-	right.addEventListener('click', (event) => {
+	right.addEventListener('click', () => {
 		if (background_slide >= taille_tab) {
 			background_slide = 0
 		}
 		else {
 			background_slide++
 		}
-		console.log(background_slide)
+		selectedDot(background_slide)
+		changeBG(background_slide)
 	});
 });
 
 left.forEach(left => {
-	left.addEventListener('click', (event) => {
+	left.addEventListener('click', () => {
 		if (background_slide <= 0) {
 			background_slide = taille_tab
 		}
 		else {
 			background_slide--
 		}
-		console.log(background_slide)
+		selectedDot(background_slide)
+		changeBG(background_slide)
 	});
 });
+
+//Boucles
+
+for (let i = slides.length; i !== 0; i--){
+	const dotdiv = document.createElement("div");
+	dotdiv.className ="dot";
+	dotsContainer.appendChild(dotdiv)
+}
+
+//Fonction appellée pour pouvoir avoir la class dot selected sur le premier dot
+
+selectedDot(background_slide)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
